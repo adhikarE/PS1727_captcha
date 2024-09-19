4759aba50d2fca979c4ff1450d6a356b058c3ca9
Kavish Shah <kksmisha@gmail.com>
2024-09-19 19:26:24 +0530

    README Indexing Bug

--- source/legacy_application.py
+++ source/legacy_application.py
@@ -10,9 +10,6 @@ config.read(config_file)
 
 DEBUG = True if input("Debugging (Y/N): ").upper() == 'Y' else False
 
-# HOST = input("Enter the static IP address you want to set: ")
-# PORT = int(input("Enter the port for legacy application: "))
-
 if input("Do you want to use the configuration file: ").upper() == "Y":
     STATIC_IP = config["Legacy_Application"]["network_interface_1"]
     PORT = int(config["Legacy_Application"]["port"])
